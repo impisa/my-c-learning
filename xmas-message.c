307ef39322faffdf77383cdfdc3abba234f5aef9
Marquezi Medeiros <poenicsfocsi@gmail.com>
2013-11-03 16:00:58 -0200

    Create xmas-message.c

---
+++ xmas-message.c
@@ -0,0 +1,43 @@
+//X-mas message//
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int numero;
+	printf("Merry X-mas! Type a number to get a massage!!\n");
+	scanf("%d",&numero);
+	switch(numero)
+{
+	case 1:
+		printf("Message 1\n"); //You only need to include a message//
+		break;
+	case 2:
+		printf("Message 2\n"); //You only need to include a message//
+		break;
+	case 3:
+		printf("Message 3\n"); //You only need to include a message//
+		break;
+	case 4:
+		printf("Message 4\n"); //You only need to include a message//
+		break;
+	case 5:
+		printf("Message 5\n"); //You only need to include a message//
+		break;
+	case 6:
+		printf("Message 6\n"); //You only need to include a message//
+		break;
+	case 7:
+		printf("Message 7\n"); //You only need to include a message//
+		break;
+	case 8:
+		printf("Message 8\n"); //You only need to include a message//
+		break;
+	case 9:
+		printf("Message 9\n"); //You only need to include a message//
+		break;
+	case 10:
+		printf("Message 10\n"); //You only need to include a message//
+		break;
+}
+}
