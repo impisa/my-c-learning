3364438d3ccfb7f3492773bc62a8caffa6885005
Kleber Shimabuku <klebershimabuku@gmail.com>
2013-11-03 19:35:56 -0200

    Allowing only alphabetic letters

--- sequence.c
+++ sequence.c
@@ -3,9 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <ctype.h>
 
 int fibonacciSequence (int n){ //Function to Fibonacci//
 	int lastTerm, penultTerm, currentTerm, i;
+
 	if (n <=1){ //Eliminating some kind of terms witch we know answer//
 	printf("The term for this integer is 0\n");
 	}
@@ -15,7 +17,7 @@ int fibonacciSequence (int n){ //Function to Fibonacci//
 	lastTerm = 1;
 	penultTerm = 0;
 	currentTerm = 2;
-	for(i=0 ; i<=n-2; i=i++){
+	for(i=0 ; i<=n-2; i++){
 	printf("The actual term from Fibonacci's Sequence to integer :%d\n", currentTerm);
 	penultTerm = lastTerm; 
 	lastTerm = currentTerm;
@@ -28,6 +30,14 @@ int main (){
 	int k, sequence;
 	printf("Type how many terms you want:");
 	scanf("%d",&k);
-	sequence = fibonacciSequence (k);
-	printf ("The %dth term from Fibonacci's Sequence is:%d\n",k, sequence);
+
+  if (!isalpha(k)) {
+    printf("are you stupid?");
+    return 0;
+
+  } else {
+    sequence = fibonacciSequence (k);
+    printf ("The %dth term from Fibonacci's Sequence is:%d\n",k, sequence);
+  }
+
 }
