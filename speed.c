8948553460e9ca29c0d8bc879368de6ae0c1b30b
impisa <poenicsfocsi@gmail.com>
2013-11-04 12:20:37 -0200

    You can now calculate the speed into a plane.

--- speed.c
+++ speed.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-int distancePlane(float x1, float x2, float y1, float y2) {
+float distancePlane(float x1, float x2, float y1, float y2) {
   float distance;
 
   distance = sqrt(pow((x2-x1),2) + pow((y2-y1),2));
@@ -11,23 +11,66 @@ int distancePlane(float x1, float x2, float y1, float y2) {
 
 int main() {
   float a1, a2, b1, b2, t1, t2, time, speed;
+  char c1[256], c2[256], c3[256], c4[256], c5[256], c6[256];
 
-  printf("Type the coordinates from the first point:\n");
-  scanf("%f%f", &a1, &b1);
-  printf("Type the coordinates from the second point:\n");
-  scanf("%f%f", &a2, &b2);
-  printf("Type the time from point one and from the second point:\n");
-  scanf("%f%f", &t1, &t2);
+  printf("Type the x coordinate from the first point:\n");
+  fgets(c1, 256, stdin);
+     while (atof(c1) == 0) {
+     printf("Are you stupid?\nType again the coodinate:");
+     fgets(c1, 256, stdin); //Giving another value to string
+	}
+  a1=atof(c1);
 
-  while (t1 < 0 && t2 < 0 && t1<t2){
-    printf("The time should be positive");
-    scanf("%f%f", &t1, &t2);
-  }
+  printf("Type the y coordinate from the first point:\n");
+  fgets(c2, 256, stdin);
+     while (atof(c2) == 0) {
+     printf("Are you stupid?\nType again the coodinate:");
+     fgets(c2, 256, stdin); //Giving another value to string
+	}
+  b1=atof(c2);
 
-  time = t2-t1;
+  printf("Type the x coordinate from the second point:\n");
+  fgets(c3, 256, stdin);
 
-  printf("The distance traveled by the particle was:%f\n", distancePlane(a1, b1, a2, b2));
-  speed = distancePlane(a1, b1, a2, b2) / time;
+    while (atof(c3) == 0) {
+    printf("Are you stupid?\nType again the coodinate:");
+    fgets(c3, 256, stdin); //Giving another value to string
+	}
+  a2=atof(c3);
+
+  printf("Type the y coordinate from the second point:\n");
+  fgets(c4, 256, stdin);
+
+    while (atof(c4) == 0) {
+    printf("Are you stupid?\nType again the coodinate:");
+    fgets(c4, 256, stdin); //Giving another value to string
+	}
+  b2=atof(c4);
+
+  
+  printf("Type the time from the fist pair of coordeates:\n");
+  fgets(c5, 256, stdin);
+
+  while (atof(c5) == 0 || atof(c5) < 0) {
+  printf("Are you stupid?\nType again the time:");
+  fgets(c5, 256, stdin); //Giving another value to string
+	}
+  t1=atof(c5);
+
+  printf("Type the time from the second pair of coordeates:\n");
+  fgets(c6, 256, stdin);
+
+    while (atof(c6) == 0 || c6 < 0 || atof(c6) <= atof(c5) ) {
+    printf("Are you stupid?\nType again the time:");
+    fgets(c6, 256, stdin); //Giving another value to string
+	}
+
+  t2=atof(c6);
+
+  time = (t2-t1);
+
+  printf("The distance traveled by the particle was:%f\n", distancePlane(a1, a2, b1, b2));
+  speed = distancePlane(a1, a2, b1, b2) / time;
   printf("The averange speed of the particle was:%f\n", speed);
 
 }
